70300a6860fbcefbb8d2abfd9e4f49241a857eb3
Anthony Curtis <tonydc1997@gmail.com>
2019-09-25 13:25:00 +0200

    feat: Create base homepage architecture

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 export default HomePage() {
   return (
-    <div>
-      
+    <div className="homepage">
+      <div className="menu">
+        <div className="menu__item">
+          <div className="content">
+            <h1 className="title">HATS</h1>
+            <span className="title__sub">SHOP NOW</span>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
