19f7777f79b0c6ab300a6be4b247e1497e510dd0
Anthony Curtis <tonydc1997@gmail.com>
2019-09-25 13:29:48 +0200

    feat: Modify homepage for visual purposes
    - Add 4 more hardcoded divs to be turned into components later.
    
    - Rewrite homepage function from expression into arrow function.
    
    - Place default export at bottom of homepage.component

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -1,16 +1,40 @@
 import React from 'react';
 
-export default HomePage() {
-  return (
-    <div className="homepage">
-      <div className="menu">
-        <div className="menu__item">
-          <div className="content">
-            <h1 className="title">HATS</h1>
-            <span className="title__sub">SHOP NOW</span>
-          </div>
+const HomePage = () => (
+  <div className="homepage">
+    <div className="menu">
+      <div className="menu__item">
+        <div className="content">
+          <h1 className="title">HATS</h1>
+          <span className="title__sub">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu__item">
+        <div className="content">
+          <h1 className="title">JACKETS</h1>
+          <span className="title__sub">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu__item">
+        <div className="content">
+          <h1 className="title">SNEAKERS</h1>
+          <span className="title__sub">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu__item">
+        <div className="content">
+          <h1 className="title">WOMENS</h1>
+          <span className="title__sub">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu__item">
+        <div className="content">
+          <h1 className="title">MENS</h1>
+          <span className="title__sub">SHOP NOW</span>
         </div>
       </div>
     </div>
-  )
-}
+  </div>
+);
+
+export default HomePage;
