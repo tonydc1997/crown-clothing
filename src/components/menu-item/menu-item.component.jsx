086c0a76b7820470849b2f228cdab0b4e83ae903
Anthony Curtis <tonydc1997@gmail.com>
2019-09-26 19:54:33 +0200

    feat: Pass imageUrl prop into menu-item.component
    - Use imageUrl within div's style attr

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title }) => (
-  <div className="menu__item">
+const MenuItem = ({ title, imageUrl }) => (
+  <div
+    style={{
+      backgroundImage: `url({imageUrl})`,
+    }}
+    className="menu__item"
+  >
     <div className="content">
       <h1 className="title">{title}</h1>
       <span className="title__sub">SHOP NOW</span>
