71373fc007bf3a578a4d6e56074038f1b8980545
Anthony Curtis <tonydc1997@gmail.com>
2019-09-26 19:09:51 +0200

    feat: Create menu-item component with apropos code

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MenuItem = () => (
+  <div className="menu__item">
+    <div className="content">
+      <h1 className="title">HATS</h1>
+      <span className="title__sub">SHOP NOW</span>
+    </div>
+  </div>
+);
+
+export default MenuItem;
