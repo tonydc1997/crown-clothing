9ceab86e76336d5dcbba6d890614e72fec644ebc
Anthony Curtis <tonydc1997@gmail.com>
2019-09-26 19:11:46 +0200

    feat: Pass props.title into menu-item.component

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const MenuItem = () => (
+const MenuItem = ({ title }) => (
   <div className="menu__item">
     <div className="content">
-      <h1 className="title">HATS</h1>
+      <h1 className="title">{title}</h1>
       <span className="title__sub">SHOP NOW</span>
     </div>
   </div>
