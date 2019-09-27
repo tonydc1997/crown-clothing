c2103c57a3df2e19036dcd5409f103d46fe3b681
Anthony Curtis <tonydc1997@gmail.com>
2019-09-27 11:50:25 +0200

    feat: Wrap App component with BrowserRouter
    - Feature occured in index.js

--- src/index.js
+++ src/index.js
@@ -4,4 +4,9 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
