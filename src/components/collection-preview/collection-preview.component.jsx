b3e0e0c53fdfa0d241636631ba40928305c5763c
Anthony Curtis <tonydc1997@gmail.com>
2019-10-01 13:42:07 +0200

    fix: Remove self-import component

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import CollectionPreview from '../collection-item/collection-item.component';
 import './collection-preview.styles.scss';
 import CollectionItem from '../collection-item/collection-item.component';
 
