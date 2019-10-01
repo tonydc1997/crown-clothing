cd9804a0fbeaa278daf6d081f7cd30d0b7b60331
Anthony Curtis <tonydc1997@gmail.com>
2019-10-01 14:23:52 +0200

    feat: Import and use Header in App.js
    - Insert Header above Switch component, ensuring it's always present.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
+import Header from './components/header/header.component';
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/shop" component={ShopPage} />
