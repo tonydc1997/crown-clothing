304df8ecc296bc57cd887571e5c301cd85d6a921
Anthony Curtis <tonydc1997@gmail.com>
2019-10-01 14:17:52 +0200

    feat: Import & use Link and Logo cmpnts in header
    - Render header w/ header container (div), linked logo to go home

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './header.styles.scss';
 
 const Header = () => (
-  
-)
+  <div className="header">
+    <Link className="logo-container" to="/">
+      <Logo className="logo" />
+    </Link>
+  </div>
+);
 
 export default Header;
