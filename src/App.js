e50c9f7e72c41f67653c3dbabc4f99a8be2e55e1
Anthony Curtis <tonydc1997@gmail.com>
2019-10-01 11:15:22 +0200

    feat: Create HatsPage in App.js for test purposes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
 function App() {
   return (
     <div>
