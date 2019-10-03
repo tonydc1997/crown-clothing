cd9f1d3c0a9b8499589282c9a93a1945e6d9ba86
Anthony Curtis <tonydc1997@gmail.com>
2019-10-03 11:29:37 +0200

    feat: Create SignIn component as a class
    - Use class as we want to hold state (email and password).
    
    - Begin building out form in the render fn().

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,2 +1,22 @@
 import React from 'react';
 import './sign-in.styles.scss';
+
+class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h2></h2>
+        <span></span>
+      </div>
+    );
+  }
+}
