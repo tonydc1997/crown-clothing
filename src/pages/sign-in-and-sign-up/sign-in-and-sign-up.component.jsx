17416d24cb1b49b5dba2168e6d6277968b0fec0d
Anthony Curtis <tonydc1997@gmail.com>
2019-10-03 11:46:55 +0200

    chore: Add classname to SignInAndSignUp Page

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -3,7 +3,7 @@ import SignIn from '../../components/sign-in/sign-in.component';
 import './sign-in-and-sign-up.styles.scss';
 
 const SignInAndSignUpPage = () => (
-  <div>
+  <div className="sign-in-and-sign-up">
     <SignIn />
   </div>
 );
