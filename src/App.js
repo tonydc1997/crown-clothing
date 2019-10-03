534cad17c10e7457b354d812ab2ccb8aefbb56cd
Anthony Curtis <tonydc1997@gmail.com>
2019-10-03 11:20:44 +0200

    chore: Import Sign In/Up Page in App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import Header from './components/header/header.component';
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
