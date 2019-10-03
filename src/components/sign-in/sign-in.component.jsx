286da212b38de05b2a4c76a85f6fc461eb67ff8a
Anthony Curtis <tonydc1997@gmail.com>
2019-10-03 11:44:14 +0200

    feat: Create handleChange() & pass to form inputs
    - Pass fn() to email and password inputs.

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -13,8 +13,12 @@ class SignIn extends React.Component {
 
   handleSubmit = event => {
     event.preventDefault();
-
     this.setState({ email: '', password: '' });
+  };
+
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
   };
 
   render() {
@@ -25,9 +29,21 @@ class SignIn extends React.Component {
         <span>Sign in with your email and password</span>
 
         <form onSubmit={this.handleSubmit}>
-          <input name="email" type="email" value={email} required />
+          <input
+            name="email"
+            type="email"
+            value={email}
+            onChange={this.handleChange}
+            required
+          />
           <label>Email</label>
-          <input name="password" type="password" value={password} required />
+          <input
+            name="password"
+            type="password"
+            value={password}
+            onChange={this.handleChange}
+            required
+          />
           <label>password</label>
 
           <input type="submit" value="Submit Form" />
