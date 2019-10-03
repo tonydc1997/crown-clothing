c761630905c337ad17a93a3780e7874e420bd170
Anthony Curtis <tonydc1997@gmail.com>
2019-10-03 12:06:08 +0200

    feat: Add label rendering logic in FormInput
    - Render a label with nice styling if a label is detected.
        - Otherwise it's null.
    
    - Pass in value prop in FormInput's params.

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './form-input.styles.scss';
 
-const FormInput = ({ handleChange, name, type, required, label }) => (
+const FormInput = ({ handleChange, name, type, required, value, label }) => (
   <div className="group">
     <input
       className="form-input"
@@ -10,6 +10,11 @@ const FormInput = ({ handleChange, name, type, required, label }) => (
       type={type}
       required={required}
     />
+    {label ? (
+      <label className={`${value.length ? `shrink` : ''} form-input-label`}>
+        {label}
+      </label>
+    ) : null}
   </div>
 );
 
