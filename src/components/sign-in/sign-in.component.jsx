80b749840835f77bd5250196e522809f1401f9da
Anthony Curtis <tonydc1997@gmail.com>
2019-10-03 12:18:29 +0200

    feat: Import and use FormInput in SignIn component
    - Import FormInput.
    
    - Replace <input../> with <FormInout .../>.
    
    - Change onChange to handleChange.
    
    - Add label for dynamic styling onFocus.
    
    - Remove label elements (in favor of props passed in <FormInput ... />).

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import FormInput from '../form-input/form-input.component';
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component {
@@ -29,23 +30,22 @@ class SignIn extends React.Component {
         <span>Sign in with your email and password</span>
 
         <form onSubmit={this.handleSubmit}>
-          <input
+          <FormInput
             name="email"
             type="email"
             value={email}
-            onChange={this.handleChange}
+            handleChange={this.handleChange}
+            label="Email"
             required
           />
-          <label>Email</label>
-          <input
+          <FormInput
             name="password"
             type="password"
             value={password}
-            onChange={this.handleChange}
+            handleChange={this.handleChange}
+            label="Password"
             required
           />
-          <label>password</label>
-
           <input type="submit" value="Submit Form" />
         </form>
       </div>
