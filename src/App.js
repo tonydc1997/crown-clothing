24d309a15ac1712c765e75d0f18f1f01dd5dd5b2
Anthony Curtis <tonydc1997@gmail.com>
2019-10-03 11:23:58 +0200

    feat: Add new SignIn <Route /> in App.js

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
