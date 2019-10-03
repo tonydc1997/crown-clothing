a9535c1f64ffa94fd9a47015372f56493268fdb3
Anthony Curtis <tonydc1997@gmail.com>
2019-10-03 11:35:32 +0200

    feat: Build out form in SignIn component
    - Include email and password labels and inputs.
    
    - Use 'submit' input.

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -12,10 +12,20 @@ class SignIn extends React.Component {
   }
 
   render() {
+    const { email, password } = this.state;
     return (
       <div className="sign-in">
         <h2>I already have an account</h2>
         <span>Sign in with your email and password</span>
+
+        <form>
+          <input name="email" type="email" value={email} required />
+          <label>Email</label>
+          <input name="password" type="password" value={password} required />
+          <label>password</label>
+
+          <input type="submit" value="Submit Form" />
+        </form>
       </div>
     );
   }
