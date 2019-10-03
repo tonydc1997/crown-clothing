c1e3cc060b9562c9c79a5f702a523fd002707ede
Anthony Curtis <tonydc1997@gmail.com>
2019-10-03 11:46:19 +0200

    feat: Import and render SignIn on SignIn Page

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import SignIn from '../../components/sign-in/sign-in.component';
 import './sign-in-and-sign-up.styles.scss';
 
-const SignInAndSignUpPage = () => <div>Sign In</div>;
+const SignInAndSignUpPage = () => (
+  <div>
+    <SignIn />
+  </div>
+);
 
 export default SignInAndSignUpPage;
