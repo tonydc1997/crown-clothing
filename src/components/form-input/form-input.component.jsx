b474cd9ebc27fbcb69176dc5d992ba78f0111b5f
Anthony Curtis <tonydc1997@gmail.com>
2019-10-03 11:58:41 +0200

    feat: Build out base functionality of FormInput
    - Export component by default.
    
    - Pass in props from SignIn Component.

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,2 +1,16 @@
 import React from 'react';
 import './form-input.styles.scss';
+
+const FormInput = ({ handleChange, name, type, required, label }) => (
+  <div className="group">
+    <input
+      className="form-input"
+      onChange={handleChange}
+      name={name}
+      type={type}
+      required={required}
+    />
+  </div>
+);
+
+export default FormInput;
