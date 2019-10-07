6e51f74df33e1e56815cff9bb7558574274bbc51
Anthony Curtis <tonydc1997@gmail.com>
2019-10-07 10:48:35 +0200

    chore: Copy/paste firebase config

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,3 +1,14 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+
+const config = {
+  apiKey: 'AIzaSyD9n33tVMg9H66tvZNZX6tl_yDhkQ1iIMI',
+  authDomain: 'crown-db-bab46.firebaseapp.com',
+  databaseURL: 'https://crown-db-bab46.firebaseio.com',
+  projectId: 'crown-db-bab46',
+  storageBucket: '',
+  messagingSenderId: '242184251880',
+  appId: '1:242184251880:web:65b711a67d0979085edc5d',
+  measurementId: 'G-X5F3SL7NS4',
+};
