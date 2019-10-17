d7a8872885371ce42f7493afa6e633fa1c23c870
Anthony Curtis <tonydc1997@gmail.com>
2019-10-17 09:40:14 +0200

    fix: Ensure Google Sign In Auth pops up onClick
    - Pass onClick prop to custom-button.component.

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './custom-button.styles.scss';
 
-const CustomButton = ({ children, type }) => (
-  <button className="custom-button" type={type}>
+const CustomButton = ({ children, type, onClick }) => (
+  <button className="custom-button" type={type} onClick={onClick}>
     {children}
   </button>
 );
