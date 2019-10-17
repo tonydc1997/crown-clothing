96e61d357dc44d81d742f5e404245d026f4bb1e9
Anthony Curtis <tonydc1997@gmail.com>
2019-10-17 09:29:11 +0200

    feat: Ensure signIn popup uses Google auth

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -20,3 +20,5 @@ export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account' });
+
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
