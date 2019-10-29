ec4bcd2313b7443cedd5f1601f206cbf44989e04
Anthony Curtis <tonydc1997@gmail.com>
2019-10-29 10:03:58 +0100

    feat: Dynamically get authUser's user id

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -16,7 +16,7 @@ const config = {
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
 
-  const userRef = firestore.doc('users/2398yr87q3rf');
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
   const snapShot = await userRef.get();
   console.log(snapShot);
 };
