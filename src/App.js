1a38c9b85c2f62391d3e386ad3f233c621728025
Anthony Curtis <tonydc1997@gmail.com>
2019-10-29 10:27:11 +0100

    feat: Grab userAuth if it exists onAuthStateChange

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { create } from 'istanbul-reports';
 
 class App extends React.Component {
   constructor() {
@@ -19,8 +20,10 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
-      createUserProfileDocument(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+      }
     });
   }
 
