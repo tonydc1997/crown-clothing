9315212938e64fcd285980f39f1b4c08e3ff9405
Anthony Curtis <tonydc1997@gmail.com>
2019-10-30 10:54:43 +0100

    feat: Store userData in the crown app
    - Replace console.log with a setState function.
    
    - setState of currentUser's id & data to match what's captured by snapshot.
    
    - If there's no userAuth, setState of currentUser to null.

--- src/App.js
+++ src/App.js
@@ -23,8 +23,20 @@ class App extends React.Component {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
         userRef.onSnapshot(snapshot => {
-          console.log(snapshot.data());
+          this.setState(
+            {
+              currentUser: {
+                id: snapshot.id,
+                ...snapshot.data(),
+              },
+            },
+            () => {
+              console.log(this.state);
+            }
+          );
         });
+      } else {
+        this.setState({ currentUser: userAuth });
       }
     });
   }
