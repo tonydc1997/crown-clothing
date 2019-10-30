03a3369a3bfb15a6fa16bacd658f8bbeb2782e90
Anthony Curtis <tonydc1997@gmail.com>
2019-10-30 11:09:34 +0100

    feat: Start building sign-up's rendered component

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -15,4 +15,16 @@ class SignUp extends React.Component {
       confirmPassword: '',
     };
   }
+
+  render() {
+    return (
+      <div className="sign-up">
+        <h2 className="title">I do not have an account</h2>
+        <span>Sign up with your email and password</span>
+        <form className="sign-up-form" onSubmit={this.handleSubmit}>
+          <FormInput></FormInput>
+        </form>
+      </div>
+    );
+  }
 }
