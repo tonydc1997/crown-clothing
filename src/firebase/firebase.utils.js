69019926385ca01e5a2c9d59b957131d2621d899
Anthony Curtis <tonydc1997@gmail.com>
2019-10-30 13:08:24 +0100

    chore: Move initializeApp fn() closer to top of file

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,8 @@ const config = {
   measurementId: 'G-X5F3SL7NS4',
 };
 
+firebase.initializeApp(config);
+
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
 
@@ -36,8 +38,6 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   }
   return userRef;
 };
-
-firebase.initializeApp(config);
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
