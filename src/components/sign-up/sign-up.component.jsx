6ba5c1560ef5e4518d3639f45adf08d7d0b39cdb
Anthony Curtis <tonydc1997@gmail.com>
2019-10-30 11:11:49 +0100

    feat: Destructure this.state's props for easy ref

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -17,6 +17,7 @@ class SignUp extends React.Component {
   }
 
   render() {
+    const { displayName, email, password, confirmPassword } = this.state;
     return (
       <div className="sign-up">
         <h2 className="title">I do not have an account</h2>
