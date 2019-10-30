3390a6423d5605dde225924f0b5f0e178e782149
Anthony Curtis <tonydc1997@gmail.com>
2019-10-30 12:13:44 +0100

    fix: Resolve file typo
    - Ensure styless is spelled styles as both a file and import statement.
    
    - GIve .sign-up class a responsive width and margin.

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 import { auth, createUserProfileDocument } from '../../firebase/firebase.utils';
-import './sign-up.styless.scss';
+import './sign-up.styles.scss';
 import SignIn from '../sign-in/sign-in.component';
 
 class SignUp extends React.Component {
