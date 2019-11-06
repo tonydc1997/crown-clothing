e1880b77e901c473ba234a0f436dbe6e3387527c
Anthony Curtis <tonydc1997@gmail.com>
2019-11-06 10:05:41 +0100

    fix: Add missing args in auth.signInWithEmail&...

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -19,7 +19,7 @@ class SignIn extends React.Component {
     const { email, password } = this.state;
 
     try {
-      await auth.signInWithEmailAndPassword;
+      await auth.signInWithEmailAndPassword(email, password);
       this.setState({ email: '', password: '' });
     } catch (err) {
       console.log(err);
