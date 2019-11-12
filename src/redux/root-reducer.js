4f946bc6cdbc38c5aef05fb6f849c33b4b676871
Anthony Curtis <tonydc1997@gmail.com>
2019-11-12 10:02:45 +0100

    feat: Set user prop to userReducer in combineReducers

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,4 +1,6 @@
 import { combineReducers } from 'redux';
 import userReducer from './user/user.reducer';
 
-export default combineReducers({});
+export default combineReducers({
+  user: userReducer,
+});
