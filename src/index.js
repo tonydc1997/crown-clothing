adb21ee21c1d028507e9b7535bf4248ede078491
Anthony Curtis <tonydc1997@gmail.com>
2019-11-12 10:13:49 +0100

    chore: Import store in index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
+import store from './redux/store';
 
 ReactDOM.render(
   <Provider>
