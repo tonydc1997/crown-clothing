6806c064ede3f1ae1c20f3f4ad539979ce471334
Anthony Curtis <tonydc1997@gmail.com>
2019-11-14 11:58:12 +0100

    feat: Render conditionally if user is signed in or not

--- src/App.js
+++ src/App.js
@@ -41,7 +41,17 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={Homepage} />
           <Route path="/shop" component={ShopPage} />
-          <Route exact path="/signin" component={SignInAndSignUpPage} />
+          <Route
+            exact
+            path="/signin"
+            render={() =>
+              this.props.currentUser ? (
+                <Redirect to="/" />
+              ) : (
+                <SignInAndSignUpPage />
+              )
+            }
+          />
         </Switch>
       </div>
     );
