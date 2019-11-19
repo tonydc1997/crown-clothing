0561b777f173ebd05584d7e3033b7647f4d7b857
Anthony Curtis <tonydc1997@gmail.com>
2019-11-19 10:58:35 +0100

    feat: Create base features of cart-icon

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,3 +1,10 @@
 import React from 'react';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import './cart-icon.style.scss';
+
+const CartIcon = () => (
+  <div>
+    <ShoppingIcon />
+    <span>0</span>
+  </div>
+);
