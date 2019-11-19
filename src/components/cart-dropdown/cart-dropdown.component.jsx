9a92ce7e26116a595997ba88a6e6aa19d61a19a5
Anthony Curtis <tonydc1997@gmail.com>
2019-11-19 11:20:15 +0100

    feat: Create cart-dropdown features

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,3 +1,10 @@
 import React from 'react';
 import CustomButton from '../custom-button/custom-button.component';
 import './cart-dropdown.styles.scss';
+
+const Cart = () => (
+  <div className="cart-dropdown">
+    <div className="cart-items" />
+    <CustomButton>GO TO CHECKOUT</CustomButton>
+  </div>
+);
