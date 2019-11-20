242534e6fb818eca807cb5199d0aabdf6335baab
Anthony Curtis <tonydc1997@gmail.com>
2019-11-20 10:14:04 +0100

    feat: toggleCart(icon)Hidden when clicked

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,7 +5,7 @@ import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import './cart-icon.style.scss';
 
 const CartIcon = ({ toggleCartHidden }) => (
-  <div className="cart-icon">
+  <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon" />
     <span className="item-count">0</span>
   </div>
