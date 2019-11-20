efa8e128e967852e2c60b022acbbb206a0b3a9d4
Anthony Curtis <tonydc1997@gmail.com>
2019-11-20 10:12:44 +0100

    feat: Pass toggleCartHidden to cartIcon

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,7 +4,7 @@ import { toggleCartHidden } from '../../redux/cart/cart.actions';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import './cart-icon.style.scss';
 
-const CartIcon = () => (
+const CartIcon = ({ toggleCartHidden }) => (
   <div className="cart-icon">
     <ShoppingIcon className="shopping-icon" />
     <span className="item-count">0</span>
