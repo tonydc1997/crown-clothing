ce7dafc06466f26f4a956ebd03c25de9a2e2367f
Anthony Curtis <tonydc1997@gmail.com>
2019-11-26 12:20:55 +0100

    feat: Display CartItem(s) when added to cart
    - fix: Ensure 'cartItems' is not capitalized.

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -6,13 +6,17 @@ import './cart-dropdown.styles.scss';
 
 const CartDropdown = ({ cartItems }) => (
   <div className="cart-dropdown">
-    <div className="cart-items" />
+    <div className="cart-items">
+      {cartItems.map(cartItem => (
+        <CartItem key={cartItem.id} item={cartItem} />
+      ))}
+    </div>
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 );
 
-const mapStateToProps = ({ cart: { CartItems } }) => ({
-  CartItems,
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems,
 });
 
 export default connect(mapStateToProps)(CartDropdown);
