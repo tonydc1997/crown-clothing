27d2238bb1c03d01841cb35117363cabb8ecbccd
Anthony Curtis <tonydc1997@gmail.com>
2019-11-26 10:36:48 +0100

    feat: Create mapDispatchToProps fn()

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import CustomButton from '../custom-button/custom-button.component';
 import { addItem } from '../../redux/cart/cart.actions';
 import './collection-item.styles.scss';
+import { dispatch } from '../../../../../../../Library/Caches/typescript/3.6/node_modules/rxjs/internal/observable/pairs';
 
 const CollectionItem = ({ id, name, imageUrl, price }) => (
   <div className="collection-item">
@@ -14,5 +15,9 @@ const CollectionItem = ({ id, name, imageUrl, price }) => (
     <CustomButton inverted>ADD TO CART</CustomButton>
   </div>
 );
+
+const mapDispatchToProps = dispatch => ({
+  addItem: item => dispatch(addItem(item)),
+});
 
 export default CollectionItem;
