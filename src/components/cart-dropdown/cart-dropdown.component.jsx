9e571463a10be588acedc83158baf20d3d611b7a
Anthony Curtis <tonydc1997@gmail.com>
2019-11-26 12:15:25 +0100

    feat: Destructure cartItems from mapState fn()

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -4,7 +4,7 @@ import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = () => (
+const CartDropdown = ({ cartItems }) => (
   <div className="cart-dropdown">
     <div className="cart-items" />
     <CustomButton>GO TO CHECKOUT</CustomButton>
