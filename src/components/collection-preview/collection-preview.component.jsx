8f24a863a3cf50edbfd45cf26fb51923d8c1e653
Anthony Curtis <tonydc1997@gmail.com>
2019-11-26 10:46:10 +0100

    feat: Pass item obj itself rather than individual props

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -8,13 +8,8 @@ const CollectionPreview = ({ title, items }) => (
     <div className="preview">
       {items
         .filter((item, index) => index < 4)
-        .map(({ id, name, imageUrl, price }) => (
-          <CollectionItem
-            key={id}
-            name={name}
-            imageUrl={imageUrl}
-            price={price}
-          />
+        .map(item => (
+          <CollectionItem key={item.id} item={item} />
         ))}
     </div>
   </div>
