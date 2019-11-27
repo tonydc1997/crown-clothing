0074b0ae6c6b2e6d264ef22a43e6aeb6ec85be9e
Anthony Curtis <tonydc1997@gmail.com>
2019-11-27 10:24:04 +0100

    feat: Create outpul selector w/ cartItems.reduce()

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -1,4 +1,11 @@
 import { createSelector } from 'reselect';
+import { create } from 'domain';
 
 const selectCart = state => state.cart;
 const selectCartItems = createSelector([selectCart], cart => cart.cartItems);
+const selectCartItemsCount = createSelector([selectCart], cartItems =>
+  cartItems.reduce(
+    (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity,
+    0
+  )
+);
