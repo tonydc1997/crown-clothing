89e8030d4a96ef7b671e9308bb67582f5fa10f8d
Anthony Curtis <tonydc1997@gmail.com>
2019-11-27 10:36:45 +0100

    fix: Ensure output selector uses previous one

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -1,13 +1,17 @@
 import { createSelector } from 'reselect';
 
 const selectCart = state => state.cart;
+
 export const selectCartItems = createSelector(
   [selectCart],
   cart => cart.cartItems
 );
-export const selectCartItemsCount = createSelector([selectCart], cartItems =>
-  cartItems.reduce(
-    (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity,
-    0
-  )
+export const selectCartItemsCount = createSelector(
+  [selectCartItems],
+  cartItems =>
+    cartItems.reduce(
+      (accumulatedQuantity, cartItem) =>
+        accumulatedQuantity + cartItem.quantity,
+      0
+    )
 );
