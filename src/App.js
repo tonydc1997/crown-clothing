41fdaf34605dfb28b68c3ad5f8d0aedcd6eb67e9
Anthony Curtis <tonydc1997@gmail.com>
2019-11-27 11:55:44 +0100

    feat: Replace redux code with reselect selector/fns

--- src/App.js
+++ src/App.js
@@ -60,8 +60,8 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser,
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
 });
 
 const mapDispatchToProps = dispatch => ({
