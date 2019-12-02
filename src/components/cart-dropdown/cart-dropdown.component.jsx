49e90ea976d85646c9d0ad4e7b43868164b4b2b9
Anthony Curtis <tonydc1997@gmail.com>
2019-12-02 10:13:02 +0100

    feat: Redirect users to checkout w/ their history

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -19,7 +19,9 @@ const CartDropdown = ({ cartItems, history }) => (
         <span className="empty-message">Your cart is empty.</span>
       )}
     </div>
-    <CustomButton>GO TO CHECKOUT</CustomButton>
+    <CustomButton onClick={() => history.push('/checkout')}>
+      GO TO CHECKOUT
+    </CustomButton>
   </div>
 );
 
