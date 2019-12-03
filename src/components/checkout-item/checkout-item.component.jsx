4fccdd42fefc113560f9c421a33e05de0b5fbdb8
Anthony Curtis <tonydc1997@gmail.com>
2019-12-03 09:34:12 +0100

    feat: Build out base functionality of CheckoutItem

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,2 +1,14 @@
 import React from 'react';
 import './checkout-item.styles.scss';
+
+const CheckoutItem = () => (
+  <div className="checkout">
+    <div className="image-container">
+      <img alt="item" />
+    </div>
+    <span className="name"></span>
+    <span className="quantity"></span>
+    <span className="price"></span>
+    <div className="remove-button">&#10005;</div>
+  </div>
+);
