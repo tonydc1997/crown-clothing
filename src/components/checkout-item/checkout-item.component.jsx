9ed9c5e8a38d7a4f4e198c038cb24fe3eaffe827
Anthony Curtis <tonydc1997@gmail.com>
2019-12-03 09:36:15 +0100

    feat: Dynamically generate cartItem w/ its props

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -4,11 +4,11 @@ import './checkout-item.styles.scss';
 const CheckoutItem = ({ cartItem: { name, imageUrl, quantity, price } }) => (
   <div className="checkout">
     <div className="image-container">
-      <img alt="item" />
+      <img src={imageUrl} alt="item" />
     </div>
-    <span className="name"></span>
-    <span className="quantity"></span>
-    <span className="price"></span>
+    <span className="name">{name}</span>
+    <span className="quantity">{quantity}</span>
+    <span className="price">{price}</span>
     <div className="remove-button">&#10005;</div>
   </div>
 );
