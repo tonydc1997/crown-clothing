b43ead9b4cb4add3b30b601b740beb051f8c90b4
Anthony Curtis <tonydc1997@gmail.com>
2019-12-04 10:56:48 +0100

    chore: Import storage folder and persistReducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,4 +1,6 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
