2d92642f26f235491bce8db3ab8047ef2711930f
Anthony Curtis <tonydc1997@gmail.com>
2019-12-04 11:07:19 +0100

    chore: Remove unneded export default code

--- src/redux/store.js
+++ src/redux/store.js
@@ -7,5 +7,3 @@ const middlewares = [logger];
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 export const persistor = persistStore(store);
-
-export default { store, persistor };
