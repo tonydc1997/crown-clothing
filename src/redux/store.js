4494f340f0086bf7760f0046317f93e322a779e7
Anthony Curtis <tonydc1997@gmail.com>
2019-12-04 10:53:55 +0100

    feat: Export (and create) store and persistor

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,6 +4,8 @@ import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 
 const middlewares = [logger];
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const persistor = persistStore(store);
+
+export default { store, persistor };
