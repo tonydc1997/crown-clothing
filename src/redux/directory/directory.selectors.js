bfecb85214eda88a646c7d8536065efb0eaa6dc9
Anthony Curtis <tonydc1997@gmail.com>
2019-12-05 10:19:49 +0100

    feat: Create & export create(d)Selector

--- src/redux/directory/directory.selectors.js
+++ src/redux/directory/directory.selectors.js
@@ -1,3 +1,8 @@
 import { createSelector } from 'react-redux';
 
 const selectDirectory = state => state.directory;
+
+export const selectDirectorySections = createSelector(
+  [selectDirectory],
+  directory => directory.sections
+);
