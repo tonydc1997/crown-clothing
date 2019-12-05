3e60261a513d29622270173d68231e2622214fd9
Anthony Curtis <tonydc1997@gmail.com>
2019-12-05 10:14:16 +0100

    feat: Transform Directory into fn() component
    - Remove class component skeleton.
    
    - Remove constructor fn() and everything within it.
    
    - Remove unneeded imports.

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,70 +1,21 @@
 import React from 'react';
 import MenuItem from '../menu-item/menu-item.component';
-import hat from '../../assets/images/hat.jpg';
-import jacket from '../../assets/images/jacket.jpg';
-import shoes from '../../assets/images/sneakers.jpg';
-import man from '../../assets/images/man.jpg';
-import woman from '../../assets/images/woman.jpg';
 import './directory.styles.scss';
 
-class Directory extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      sections: [
-        {
-          title: 'hats',
-          imageUrl: `${hat}`,
-          id: 1,
-          linkUrl: 'shop/hats',
-        },
-        {
-          title: 'jackets',
-          imageUrl: `${jacket}`,
-          id: 2,
-          linkUrl: 'shop/jackets',
-        },
-        {
-          title: 'sneakers',
-          imageUrl: `${shoes}`,
-          id: 3,
-          linkUrl: 'shop/sneakers',
-        },
-        {
-          title: 'womens',
-          imageUrl: `${woman}`,
-          size: 'large',
-          id: 4,
-          linkUrl: 'shop/womens',
-        },
-        {
-          title: 'mens',
-          imageUrl: `${man}`,
-          size: 'large',
-          id: 5,
-          linkUrl: 'shop/mens',
-        },
-      ],
-    };
-  }
-
-  render() {
-    const { sections } = this.state;
-    return (
-      <div className="directory__menu">
-        {sections.map(({ id, title, imageUrl, size, linkUrl }) => (
-          <MenuItem
-            key={id}
-            title={title}
-            imageUrl={imageUrl}
-            size={size}
-            linkUrl={linkUrl}
-          />
-        ))}
-      </div>
-    );
-  }
-}
+const Directory = ({ sections }) => {
+  return (
+    <div className="directory__menu">
+      {sections.map(({ id, title, imageUrl, size, linkUrl }) => (
+        <MenuItem
+          key={id}
+          title={title}
+          imageUrl={imageUrl}
+          size={size}
+          linkUrl={linkUrl}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default Directory;
