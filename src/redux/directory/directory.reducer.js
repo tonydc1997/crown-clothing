da58c006b9530e1b7e3b504f5959423ad875bc30
Anthony Curtis <tonydc1997@gmail.com>
2019-12-05 09:55:49 +0100

    feat: Create initial state of directory.reducer

---
+++ src/redux/directory/directory.reducer.js
@@ -0,0 +1,36 @@
+const INITIAL_STATE = {
+  sections: [
+    {
+      title: 'hats',
+      imageUrl: `${hat}`,
+      id: 1,
+      linkUrl: 'shop/hats',
+    },
+    {
+      title: 'jackets',
+      imageUrl: `${jacket}`,
+      id: 2,
+      linkUrl: 'shop/jackets',
+    },
+    {
+      title: 'sneakers',
+      imageUrl: `${shoes}`,
+      id: 3,
+      linkUrl: 'shop/sneakers',
+    },
+    {
+      title: 'womens',
+      imageUrl: `${woman}`,
+      size: 'large',
+      id: 4,
+      linkUrl: 'shop/womens',
+    },
+    {
+      title: 'mens',
+      imageUrl: `${man}`,
+      size: 'large',
+      id: 5,
+      linkUrl: 'shop/mens',
+    },
+  ],
+};
