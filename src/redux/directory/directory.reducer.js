65f2f803c0f0a0da09b2526dd9bd84d960073ff6
Anthony Curtis <tonydc1997@gmail.com>
2019-12-05 10:13:02 +0100

    feat: Replace string interpolation with explicit paths

--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -2,32 +2,32 @@ const INITIAL_STATE = {
   sections: [
     {
       title: 'hats',
-      imageUrl: `${hat}`,
+      imageUrl: '../../assets/images/hat.jpg',
       id: 1,
       linkUrl: 'shop/hats',
     },
     {
       title: 'jackets',
-      imageUrl: `${jacket}`,
+      imageUrl: '../../assets/images/jacket.jpg',
       id: 2,
       linkUrl: 'shop/jackets',
     },
     {
       title: 'sneakers',
-      imageUrl: `${shoes}`,
+      imageUrl: '../../assets/images/sneakers.jpg',
       id: 3,
       linkUrl: 'shop/sneakers',
     },
     {
       title: 'womens',
-      imageUrl: `${woman}`,
+      imageUrl: '../../assets/images/woman.jpg',
       size: 'large',
       id: 4,
       linkUrl: 'shop/womens',
     },
     {
       title: 'mens',
-      imageUrl: `${man}`,
+      imageUrl: '../../assets/images/man.jpg',
       size: 'large',
       id: 5,
       linkUrl: 'shop/mens',
