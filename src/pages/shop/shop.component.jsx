f3693156e1b7d9dc0d68f52c7ffe9b1b097301f1
Anthony Curtis <tonydc1997@gmail.com>
2019-12-10 10:29:43 +0100

    chore: Import 3 deps in shop.component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { selectShopItems } from '../../redux/shop/shop.selectors';
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 import './shop.styles.scss';
 
