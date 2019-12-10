36760cf82810a63dbe83b1b1ec81ead1328b8f19
Anthony Curtis <tonydc1997@gmail.com>
2019-12-10 11:07:33 +0100

    chore: Import shop.selector

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+import { selectCollections } from '../../redux/shop/shop.selectors';
 import CollectionPreview from '../collection-preview/collection-preview.component';
 import './collections-overview.styles.scss';
 
