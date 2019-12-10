3e094359be5d76bff67dbd8ad6077df084b235df
Anthony Curtis <tonydc1997@gmail.com>
2019-12-10 10:21:58 +0100

    feat: Transform ShopPage cmpnt into functional one

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,17 +2,12 @@ import React from 'react';
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 import './shop.styles.scss';
 
-class ShopPage extends React.Component {
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop">
-        {collections.map(({ id, title, items }) => (
-          <CollectionPreview key={id} title={title} items={items} />
-        ))}
-      </div>
-    );
-  }
-}
+const ShopPage = () => (
+  <div className="shop">
+    {collections.map(({ id, title, items }) => (
+      <CollectionPreview key={id} title={title} items={items} />
+    ))}
+  </div>
+);
 
 export default ShopPage;
