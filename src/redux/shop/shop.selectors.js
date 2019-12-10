6a01438266e1ebb25f7daec2bab4894279af059f
Anthony Curtis <tonydc1997@gmail.com>
2019-12-10 10:26:43 +0100

    feat: Create shop.selectors

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,1 +1,8 @@
 import { createSelectors } from 'reselect';
+
+const selectShop = state => state.shop;
+
+export const selectShopItems = createSelectors(
+  [selectShop],
+  shop => shop.shopItems
+);
