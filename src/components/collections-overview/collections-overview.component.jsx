27dd508653b0d8cd0eb8d82b493d5a14b775cfaf
Anthony Curtis <tonydc1997@gmail.com>
2019-12-10 11:03:25 +0100

    chore: Import fns from redux & reselect, CollectionPreview

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -1,2 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import CollectionPreview from '../collection-preview/collection-preview.component';
 import './collections-overview.styles.scss';
