ae3aeb0c57bea81f486c5bbd2dfd241722160221
Anthony Curtis <tonydc1997@gmail.com>
2019-12-10 10:22:38 +0100

    feat: Grab/destructure collections info

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 import './shop.styles.scss';
 
-const ShopPage = () => (
+const ShopPage = ({ collections }) => (
   <div className="shop">
     {collections.map(({ id, title, items }) => (
       <CollectionPreview key={id} title={title} items={items} />
