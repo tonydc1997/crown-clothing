e5d463d4b2125f41ab43286205fb635deb4be5fd
Anthony Curtis <tonydc1997@gmail.com>
2019-12-10 11:04:20 +0100

    feat: Create skeleton of collections-overview

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -3,3 +3,7 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import CollectionPreview from '../collection-preview/collection-preview.component';
 import './collections-overview.styles.scss';
+
+const CollectionsOverview = ({ collections }) => (
+  <div className="collections-overview"></div>
+);
