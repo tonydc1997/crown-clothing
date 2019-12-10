71b1f1be7ebd53409077dfe4157c194b7baa3ce7
Anthony Curtis <tonydc1997@gmail.com>
2019-12-10 11:05:57 +0100

    chore: Remove unneeded imports in ShopPage

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-import { selectCollections } from '../../redux/shop/shop.selectors';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 import './shop.styles.scss';
 
 const ShopPage = ({ collections }) => <div className="shop"></div>;
