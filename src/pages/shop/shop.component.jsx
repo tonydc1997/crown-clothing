ec4b816dd8a8d633abf1c4ce037a33bf0d5cf7b0
Anthony Curtis <tonydc1997@gmail.com>
2019-12-10 10:20:31 +0100

    chore: Remove unused code in shop.component
    - Remove SHOP_DATA import stmt.
    
    - Remove constructor fn().

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,17 +1,8 @@
 import React from 'react';
-import SHOP_DATA from './shop.data';
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 import './shop.styles.scss';
 
 class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
-
   render() {
     const { collections } = this.state;
     return (
