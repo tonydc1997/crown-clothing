b47f5b3394aa4e7cd196151892930009dc13ea9c
Anthony Curtis <tonydc1997@gmail.com>
2019-12-11 08:42:11 +0100

    feat: Create skeleton of CategoryPage component

--- src/pages/category/category.component.jsx
+++ src/pages/category/category.component.jsx
@@ -1,3 +1,9 @@
 import React from 'react';
 import CollectionItem from '../../components/collection-item/collection-item.component';
 import './category.styles.scss';
+
+const CategoryPage = () => (
+  <div className="category">
+    <h2>CATEGORY</h2>
+  </div>
+);
