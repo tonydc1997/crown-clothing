103695b010d8c2993d6074b038f6b64faff69381
Anthony Curtis <tonydc1997@gmail.com>
2019-12-11 09:10:35 +0100

    feat: Create new shopSelector

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -14,3 +14,10 @@ export const selectCollections = createSelector(
   [selectShop],
   shop => shop.collections
 );
+
+export const selectCollection = collectionUrlParam =>
+  createSelector([selectCollections], collections =>
+    collections.find(
+      collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
+    )
+  );
