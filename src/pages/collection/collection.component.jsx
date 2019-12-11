0def7cc2d629c8c4eabc92752deccefb66aeb469
Anthony Curtis <tonydc1997@gmail.com>
2019-12-11 09:22:49 +0100

    feat: Connect CollectionPage to redux
    - Connect CollectionPage w/ mapStateToProps fn().
    
    - Grab and console.log(collection) to see if we're grabbing it correctly.

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -4,8 +4,8 @@ import CollectionItem from '../../components/collection-item/collection-item.com
 import { selectCollection } from '../../redux/shop/shop.selectors';
 import './collection.styles.scss';
 
-const CollectionPage = ({ match }) => {
-  console.log(match);
+const CollectionPage = ({ collection }) => {
+  console.log(collection);
   return (
     <div className="collection-page">
       <h2>COLLECTION</h2>
@@ -17,4 +17,4 @@ const mapStateToProps = (state, ownProps) => ({
   collection: selectCollection(ownProps.match.params.collectionId)(state),
 });
 
-export default CollectionPage;
+export default connect(mapStateToProps)(CollectionPage);
