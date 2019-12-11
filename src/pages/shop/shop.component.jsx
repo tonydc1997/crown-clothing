50ae2350137bd5564c22fc3b2389f3f176faf6a6
Anthony Curtis <tonydc1997@gmail.com>
2019-12-11 09:16:45 +0100

    chore: Console.log(match) to view props in browser

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -4,11 +4,14 @@ import CollectionsOverview from '../../components/collections-overview/collectio
 import CollectionPage from '../collection/collection.component';
 import './shop.styles.scss';
 
-const ShopPage = ({ match }) => (
-  <div className="shop">
-    <Route exact path={`${match.path}`} component={CollectionsOverview} />
-    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-  </div>
-);
+const ShopPage = ({ match }) => {
+  console.log(match);
+  return (
+    <div className="shop">
+      <Route exact path={`${match.path}`} component={CollectionsOverview} />
+      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+    </div>
+  );
+};
 
 export default ShopPage;
