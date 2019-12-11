8fe926487683876f6ae4961d6d0b837b761300f3
Anthony Curtis <tonydc1997@gmail.com>
2019-12-11 10:59:56 +0100

    chore: Remove console.log in shopPage

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,16 +2,12 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component';
-import './shop.styles.scss';
 
-const ShopPage = ({ match }) => {
-  console.log(match);
-  return (
-    <div className="shop">
-      <Route exact path={`${match.path}`} component={CollectionsOverview} />
-      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-    </div>
-  );
-};
+const ShopPage = ({ match }) => (
+  <div className="shop">
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+  </div>
+);
 
 export default ShopPage;
