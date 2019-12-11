edcf9822d28c1d29c0f81188946a25863dac51d8
Anthony Curtis <tonydc1997@gmail.com>
2019-12-11 08:28:54 +0100

    chore: Import Route component into ShopPage

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import './shop.styles.scss';
 
