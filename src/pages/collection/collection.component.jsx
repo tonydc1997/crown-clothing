085c805d58ec1ed7bf380266445405eebc2b0274
Anthony Curtis <tonydc1997@gmail.com>
2019-12-11 10:18:41 +0100

    feat: Map over items in Collection to display them

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -9,6 +9,11 @@ const CollectionPage = ({ collection }) => {
   return (
     <div className="collection-page">
       <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
     </div>
   );
 };
