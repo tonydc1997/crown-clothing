72ac3648a778610674097fc01804e08bcdb723f2
Anthony Curtis <tonydc1997@gmail.com>
2019-12-12 10:50:46 +0100

    chore: Rename StripeButton for explicitness

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
 
-const StripeButton = ({ price }) => {
+const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_jFdbVW4LoSKQfHDZGvGOjUvU00AgEyFRcT';
   const onToken = token => {
@@ -25,4 +25,4 @@ const StripeButton = ({ price }) => {
   );
 };
 
-export default StripeButton;
+export default StripeCheckoutButton;
