fc6c96176fd9980949f56bc740a1cfda278f64f7
Anthony Curtis <tonydc1997@gmail.com>
2019-12-12 10:47:49 +0100

    feat: Create test fn() for token

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -4,6 +4,10 @@ import StripeCheckout from 'react-stripe-checkout';
 const StripeButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_jFdbVW4LoSKQfHDZGvGOjUvU00AgEyFRcT';
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Successful');
+  };
 
   return (
     <StripeCheckout
@@ -15,6 +19,7 @@ const StripeButton = ({ price }) => {
       description={`Your total is $${price}`}
       amount={priceForStripe}
       panelLabel="Pay Now"
+      token={onToken}
     />
   );
 };
