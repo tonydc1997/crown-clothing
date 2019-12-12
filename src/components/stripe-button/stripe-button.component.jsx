956889524500535825e4f534c93d292de48fdf01
Anthony Curtis <tonydc1997@gmail.com>
2019-12-12 10:46:21 +0100

    feat: Render StripeCheckout in StripeButton

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -4,4 +4,17 @@ import StripeCheckout from 'react-stripe-checkout';
 const StripeButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_jFdbVW4LoSKQfHDZGvGOjUvU00AgEyFRcT';
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CROWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://sendeyo.com/up/d/f3eb2117da"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+    />
+  );
 };
