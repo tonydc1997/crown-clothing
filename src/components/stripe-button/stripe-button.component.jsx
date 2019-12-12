ec7a84dd4bb47fcf1ef057d053386145bfd8f763
Anthony Curtis <tonydc1997@gmail.com>
2019-12-12 10:40:30 +0100

    feat: Create StripeButton component

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,2 +1,7 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+
+const StripeButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_jFdbVW4LoSKQfHDZGvGOjUvU00AgEyFRcT';
+};
