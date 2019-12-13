7c19e5ea422642d2db630df992fc75bba2a14f70
Anthony Curtis <tonydc1997@gmail.com>
2019-12-13 09:29:49 +0100

    feat: Set middlewares to an empty array

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,7 @@ import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 
-const middlewares = [logger];
+const middlewares = [];
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 export const persistor = persistStore(store);
