7bdda1965f6862ea559b8ec93b242455c1ffe3f4
Anthony Curtis <tonydc1997@gmail.com>
2019-12-13 10:34:11 +0100

    feat: Move scss .logo-container styles to styledComponents vers.

--- src/components/header/header.styles.jsx
+++ src/components/header/header.styles.jsx
@@ -8,3 +8,44 @@ const HeaderContainer = styled.div`
   justify-content: space-between;
   margin-bottom: 25px;
 `;
+
+const LogoContainer = styled(Link)`
+  text-align: center;
+  height: 58%;
+  left: 3.5rem;
+  top: 1rem;
+  width: 70px;
+  position: relative;
+
+  &::before,
+  &::after {
+    content: '';
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    width: 2px;
+    background-color: #2d2d2d;
+    transform: scaleY(0);
+    transition: transform 0.5s ease;
+  }
+
+  &::before {
+    left: -8px;
+    transform-origin: center top;
+  }
+
+  &:hover::before {
+    transform-origin: center bottom;
+    transform: scaleY(1);
+  }
+
+  &::after {
+    right: -8px;
+    transform-origin: center bottom;
+  }
+
+  &:hover::after {
+    transform-origin: center top;
+    transform: scaleY(1);
+  }
+`;
