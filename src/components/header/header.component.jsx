ccc7416bd2586f2b445f82011c9c078403454ad5
Anthony Curtis <tonydc1997@gmail.com>
2019-12-13 11:04:47 +0100

    feat: Ensure OptionLink rendered as a <div />

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -24,7 +24,9 @@ const Header = ({ currentUser, hidden }) => (
       <OptionLink to="/shop">SHOP</OptionLink>
       <OptionLink to="/shop">CONTACT</OptionLink>
       {currentUser ? (
-        <OptionLink onClick={() => auth.signOut()}>SIGN OUT</OptionLink>
+        <OptionLink as="div" onClick={() => auth.signOut()}>
+          SIGN OUT
+        </OptionLink>
       ) : (
         <OptionLink to="/signin">SIGN IN</OptionLink>
       )}
