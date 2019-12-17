a1b55d92e419683b9a56abe95eb0956088b10d58
Anthony Curtis <tonydc1997@gmail.com>
2019-12-17 13:26:07 +0100

    chore: Move scss style to styledComponent vers.
    - Export default styledComponent.

--- src/components/collections-overview/collections-overview.styles.jsx
+++ src/components/collections-overview/collections-overview.styles.jsx
@@ -1,1 +1,9 @@
 import styled from 'styled-components';
+import { CollectionPreviewContainer } from '../collection-preview/collection-preview.styles';
+
+const CollectionsOverviewContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export default CollectionPreviewContainer;
