7b7185cad6c3a444170fbb7e0e54634962c31c42
Anthony Curtis <tonydc1997@gmail.com>
2019-12-17 09:56:31 +0100

    style: Create base btnStyles

--- src/components/custom-button/custom-button.styles.jsx
+++ src/components/custom-button/custom-button.styles.jsx
@@ -1,5 +1,31 @@
 import styled, { css } from 'styled-components';
 
+const buttonStyles = css`
+  background-image: linear-gradient(
+    to top,
+    #232323,
+    #303030,
+    #3d3d3d,
+    #4b4b4b,
+    #5a5959
+  );
+  border: 1px solid #2e2e2e;
+  color: #e7e7e7;
+
+  &:hover {
+    background-image: linear-gradient(
+      to top,
+      #191919,
+      #252525,
+      #313131,
+      #3e3e3e,
+      #4c4c4c
+    );
+    color: white;
+    border: 1px solid black;
+  }
+`;
+
 const invertedButtonStyles = css`
   background-image: linear-gradient(
     to top,
@@ -67,33 +93,10 @@ export const CustomButtonContainer = styled.button`
   line-height: 50px;
   padding: 0 35px;
   font-size: 15px;
-  background-image: linear-gradient(
-    to top,
-    #232323,
-    #303030,
-    #3d3d3d,
-    #4b4b4b,
-    #5a5959
-  );
-  color: #e7e7e7;
   text-transform: uppercase;
   font-family: 'Open Sans Condensed';
   font-weight: bolder;
-  border: 1px solid #2e2e2e;
   cursor: pointer;
   display: flex;
   justify-content: center;
-
-  &:hover {
-    background-image: linear-gradient(
-      to top,
-      #191919,
-      #252525,
-      #313131,
-      #3e3e3e,
-      #4c4c4c
-    );
-    color: white;
-    border: 1px solid black;
-  }
 `;
