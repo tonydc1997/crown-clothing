8d7bcef688dd02444d8ce9da0b25c19cfc15d1e8
Anthony Curtis <tonydc1997@gmail.com>
2019-12-17 12:42:28 +0100

    chore: Import CustomButton to styled component file

--- src/components/cart-dropdown/cart-dropdown.styles.jsx
+++ src/components/cart-dropdown/cart-dropdown.styles.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import CustomButton from '../custom-button/custom-button.component';
 
 export const CartDropdownContainer = styled.div`
   position: absolute;
