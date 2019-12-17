b7e62666bdeeb1b1f67c7e5c202da12c4a28587b
Anthony Curtis <tonydc1997@gmail.com>
2019-12-17 13:34:34 +0100

    fix: Ensure correct component is exported
    - Remove import statement.

--- src/components/collections-overview/collections-overview.styles.jsx
+++ src/components/collections-overview/collections-overview.styles.jsx
@@ -1,9 +1,8 @@
 import styled from 'styled-components';
-import { CollectionPreviewContainer } from '../collection-preview/collection-preview.styles';
 
 const CollectionsOverviewContainer = styled.div`
   display: flex;
   flex-direction: column;
 `;
 
-export default CollectionPreviewContainer;
+export default CollectionsOverviewContainer;
