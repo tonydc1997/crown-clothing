86f53154470ab2a82c3310efac1b852e96ac7848
Anthony Curtis <tonydc1997@gmail.com>
2019-12-17 10:33:22 +0100

    feat: Create/move collection-item styles to new component

--- src/components/collection-item/collection-item.styles.jsx
+++ src/components/collection-item/collection-item.styles.jsx
@@ -1,1 +1,10 @@
 import styled from 'styled-components';
+
+export const CollectionItemContainer = styled.div`
+  width: 20vw;
+  display: flex;
+  flex-direction: column;
+  height: 350px;
+  align-items: center;
+  position: relative;
+`;
