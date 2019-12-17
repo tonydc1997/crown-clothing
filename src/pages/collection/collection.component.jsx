d1372f9be49063387d1025ed3e2493c2d964c3d4
Anthony Curtis <tonydc1997@gmail.com>
2019-12-17 13:48:54 +0100

    feat: Import and use styledComponents

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -2,19 +2,26 @@ import React from 'react';
 import { connect } from 'react-redux';
 import CollectionItem from '../../components/collection-item/collection-item.component';
 import { selectCollection } from '../../redux/shop/shop.selectors';
-import './collection.styles.scss';
+import {
+  CollectionPageContainer,
+  CollectionItemsContainer,
+  ItemsContainer,
+} from './collection.styles';
+import { CollectionItemContainer } from '../../components/collection-item/collection-item.styles';
 
 const CollectionPage = ({ collection }) => {
   const { title, items } = collection;
   return (
-    <div className="collection-page">
-      <h2 className="title">{title}</h2>
-      <div className="items">
+    <CollectionPageContainer className="collection-page">
+      <CollectionItemContainer className="title">
+        {title}
+      </CollectionItemContainer>
+      <ItemsContainer className="items">
         {items.map(item => (
           <CollectionItem key={item.id} item={item} />
         ))}
-      </div>
-    </div>
+      </ItemsContainer>
+    </CollectionPageContainer>
   );
 };
 
