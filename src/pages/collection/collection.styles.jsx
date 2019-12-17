74ed57d10383f1c0622e696d224d6f8f186f4ef5
Anthony Curtis <tonydc1997@gmail.com>
2019-12-17 13:46:56 +0100

    feat/chore: Move scss styles to styledComponent vers.

--- src/pages/collection/collection.styles.jsx
+++ src/pages/collection/collection.styles.jsx
@@ -1,1 +1,18 @@
 import styled from 'styled-components';
+
+export const CollectionPageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const TitleContainer = styled.div`
+  color: #102a43;
+  font-size: 38px;
+  margin: 0 auto 30px;
+`;
+
+export const CollectionItemsContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-gap: 10px;
+`;
