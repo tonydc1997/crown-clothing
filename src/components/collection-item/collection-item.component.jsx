abb6dbc9932169a33685e24c9ef756b2f73eb748
Anthony Curtis <tonydc1997@gmail.com>
2019-12-17 10:56:56 +0100

    chore: Import styledComponents to collectionItem
    - Remove unneeded CustomButton import

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import CustomButton from '../custom-button/custom-button.component';
 import { addItem } from '../../redux/cart/cart.actions';
-import './collection-item.styles.scss';
+import {
+  CollectionItemContainer,
+  BackgroundImage,
+  CollectionFooterContainer,
+  NameContainer,
+  PriceContainer,
+  AddButton,
+} from './collection-item.styles';
 
 const CollectionItem = ({ item, addItem }) => {
   const { name, price, imageUrl } = item;
