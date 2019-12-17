77eb5c1811bf0110fc4771376b28ef1e5bbb537f
Anthony Curtis <tonydc1997@gmail.com>
2019-12-17 13:58:33 +0100

    feat: Create and export default styledComponent

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.styles.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.styles.jsx
@@ -1,1 +1,11 @@
 import styled from 'styled-components';
+
+const SignInAndSignUpContainer = styled.div`
+  padding: 25px;
+  width: 60vw;
+  display: flex;
+  justify-content: space-between;
+  margin: 2rem auto;
+`;
+
+export default SignInAndSignUpContainer;
