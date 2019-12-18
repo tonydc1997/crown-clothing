b6edd1552e15f9ff4243dbe0adacd65510712af8
Anthony Curtis <tonydc1997@gmail.com>
2019-12-18 10:22:06 +0100

    chore: Move scss styles to styledComponent file

--- src/components/sign-in/sign-in.styles.jsx
+++ src/components/sign-in/sign-in.styles.jsx
@@ -1,1 +1,24 @@
 import styled from 'styled-components';
+
+export const SignInContainer = styled.div`
+  width: 28vw;
+  display: flex;
+  flex-direction: column;
+  margin-right: 4rem;
+`;
+
+export const Title = styled.h2`
+  color: #243b53;
+  font-size: 28px;
+  margin-top: 17px;
+`;
+
+export const Content = styled.span`
+  color: #486581;
+  font-size: 18px;
+`;
+
+export const ButtonsContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
