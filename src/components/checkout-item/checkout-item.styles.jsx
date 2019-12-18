1730e683ddb9ca3020c4b5c578511f1a6b97c9e8
Anthony Curtis <tonydc1997@gmail.com>
2019-12-18 09:23:15 +0100

    feat: Move some scss styles to styledComponent vers.
    - Move styles for checkout-item, image-container, and remove-button

--- src/components/checkout-item/checkout-item.styles.jsx
+++ src/components/checkout-item/checkout-item.styles.jsx
@@ -1,1 +1,27 @@
 import styled from 'styled-components';
+
+export const CheckoutItemContainer = styled.div`
+  width: 100%;
+  display: flex;
+  min-height: 100px;
+  border-bottom: 2px solid hsl(210, 23%, 95%);
+  padding: 15px 0;
+  font-size: 20px;
+  align-items: center;
+`;
+
+export const ImageContainer = styled.div`
+  width: 23%;
+  padding-right: 15px;
+
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+export const RemoveButton = styled.div`
+  padding-left: 12px;
+  cursor: pointer;
+  color: #829ab1;
+`;
