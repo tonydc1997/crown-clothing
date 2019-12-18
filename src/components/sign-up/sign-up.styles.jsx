ab767691baf5eb6855be62a12112ddbee3b14dac
Anthony Curtis <tonydc1997@gmail.com>
2019-12-18 11:01:50 +0100

    chore: Move scss styles to styledComponent file

--- src/components/sign-up/sign-up.styles.jsx
+++ src/components/sign-up/sign-up.styles.jsx
@@ -1,1 +1,18 @@
 import styled from 'styled-components';
+
+export const SignUpContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 28vw;
+
+  span {
+    color: #486581;
+    font-size: 18px;
+  }
+`;
+
+export const Title = styled.h2`
+  margin: 20px 0;
+  color: #243b53;
+  font-size: 28px;
+`;
