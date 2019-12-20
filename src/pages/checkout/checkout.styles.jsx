383594fc08c9df7a9e1c062560f1525bd4c678b8
Anthony Curtis <tonydc1997@gmail.com>
2019-12-20 14:01:29 +0100

    chore: Move CheckoutPage styles to styledComp.... vers.

--- src/pages/checkout/checkout.styles.jsx
+++ src/pages/checkout/checkout.styles.jsx
@@ -1,1 +1,15 @@
 import styled from 'styled-components';
+
+const CheckoutPageContainer = styled.div`
+  width: 55%;
+  min-height: 90vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 50px auto 0;
+
+  button {
+    margin-left: auto;
+    margin-top: 50px;
+  }
+`;
