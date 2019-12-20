29441df970665af83c17a7d7dfe8d7a548d0e484
Anthony Curtis <tonydc1997@gmail.com>
2019-12-20 14:05:16 +0100

    chore: Export & move scss styles to styledComp.. vers.

--- src/pages/checkout/checkout.styles.jsx
+++ src/pages/checkout/checkout.styles.jsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-const CheckoutPageContainer = styled.div`
+export const CheckoutPageContainer = styled.div`
   width: 55%;
   min-height: 90vh;
   display: flex;
@@ -12,4 +12,43 @@ const CheckoutPageContainer = styled.div`
     margin-left: auto;
     margin-top: 50px;
   }
+`;
+
+export const CheckoutHeader = styled.div`
+  width: 100%;
+  padding: 10px 0;
+  display: flex;
+  justify-content: space-between;
+  border-bottom: 2px solid hsl(210, 23%, 95%);
+`;
+
+export const HeaderBlock = styled.div`
+  color: #102a43;
+  font-size: 18px;
+  text-transform: capitalize;
+  width: 23%;
+
+  &:last-child {
+    width: 8%;
+  }
+`;
+
+export const Total = styled.div`
+  margin-top: 30px;
+  margin-left: auto;
+  font-size: 36px;
+  color: #829ab1;
+`;
+
+export const TotolPrice = styled.span`
+  font-size: 48px;
+  font-weight: 700;
+  color: #243b53;
+`;
+
+export const TestCardWarning = styled.div`
+  text-align: center;
+  margin-top: 40px;
+  font-size: 24px;
+  color: #eb1313;
 `;
