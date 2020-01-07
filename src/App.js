06f99a1d1abea21fdff6f75f0d68fed7aed17b04
Anthony Curtis <tonydc1997@gmail.com>
2020-01-07 12:07:00 +0100

    chore: Import needed deps
    - Import new firebase.utils fn().
    
    - Import redux shop selector.

--- src/App.js
+++ src/App.js
@@ -10,9 +10,14 @@ import ShopPage from './pages/shop/shop.component';
 import CheckoutPage from './pages/checkout/checkout.component';
 
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import {
+  auth,
+  createUserProfileDocument,
+  addCollectionAndDocuments,
+} from './firebase/firebase.utils';
 import setCurrentUser from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
+import { selectCollectionsForPreview } from './redux/shop/shop.selectors';
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
