ad65a5111a0a746efbb0cca34e5c54d7d158d7c2
Anthony Curtis <tonydc1997@gmail.com>
2020-03-04 10:49:05 -0500

    chore: Ensure collections array is fully converted to an obj

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -14,7 +14,8 @@ class ShopPage extends React.Component {
   componentDidMount() {
     const collectionRef = firestore.collection('collections');
     collectionRef.onSnapshot(async snapshot => {
-      convertCollectionsSnapshotToMap(snapshot);
+      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+      console.log(collectionsMap);
     });
   }
 
