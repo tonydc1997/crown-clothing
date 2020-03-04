43d970cc76961c04afc9c2406ab23c150f811ebc
Anthony Curtis <tonydc1997@gmail.com>
2020-03-04 10:31:10 -0500

    feat: Flesh out CollectionConversion fn()
    - Declare var that maps over collections arr.
    
    - Return new object with doc.data() (*think id, title, items) and routeName prop.

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -51,7 +51,17 @@ export const addCollectionAndDocuments = async (
   });
   return await batch.commit();
 };
-const convertCollectionsSnapshotToMap();
+const convertCollectionsSnapshotToMap = collections => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items,
+    };
+  });
+};
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
