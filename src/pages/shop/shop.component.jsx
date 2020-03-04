d405e9cf53f04b59950253ca24af6c3b74d91d24
Anthony Curtis <tonydc1997@gmail.com>
2020-03-04 10:08:51 -0500

    feat: Make ShopPage a class-based component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -4,11 +4,18 @@ import CollectionsOverview from '../../components/collections-overview/collectio
 import CollectionPage from '../collection/collection.component';
 import ShopPageContainer from './shop.styles';
 
-const ShopPage = ({ match }) => (
-  <ShopPageContainer>
-    <Route exact path={`${match.path}`} component={CollectionsOverview} />
-    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-  </ShopPageContainer>
-);
+class ShopPage extends React.Component {
+  render() {
+    return (
+      <ShopPageContainer>
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route
+          path={`${match.path}/:collectionId`}
+          component={CollectionPage}
+        />
+      </ShopPageContainer>
+    );
+  }
+}
 
 export default ShopPage;
