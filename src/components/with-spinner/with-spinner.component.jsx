af6f26b966c8d5cf7c94e27c8d23d264d095c531
Anthony Curtis <tonydc1997@gmail.com>
2020-03-06 08:13:36 -0500

    feat: Make HOC more explicit for clarity
    - Wrap functionality into a Spinner var.
    
    - Return Spinner.

--- src/components/with-spinner/with-spinner.component.jsx
+++ src/components/with-spinner/with-spinner.component.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { SpinnerOverlay, SpinnerContainer } from './with-spinner.styles';
 
-const WithSpinner = WrappedComponent => ({ isLoading, ...otherProps }) => {
-  return isLoading ? (
-    <SpinnerOverlay>
-      <SpinnerContainer />
-    </SpinnerOverlay>
-  ) : (
-    <WrappedComponent {...otherProps} />
-  );
+const WithSpinner = WrappedComponent => {
+  const Spinner = ({ isLoading, ...otherProps }) => {
+    return isLoading ? (
+      <SpinnerOverlay>
+        <SpinnerContainer />
+      </SpinnerOverlay>
+    ) : (
+      <WrappedComponent {...otherProps} />
+    );
+  };
+  return Spinner;
 };
 
 export default WithSpinner;
