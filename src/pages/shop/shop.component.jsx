c70821e37c59ce602380ac9e0c903deef4872273
Anthony Curtis <tonydc1997@gmail.com>
2020-03-10 11:53:33 -0400

    chore: Remove unused code in shop.component
    - Remove:
      - Firebase import
      - Everything inside componentDidMount(){...}
      - [loading] State and its references
      - UnsubscribeFromSnapshot = null

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
-import {
-  firestore,
-  convertCollectionsSnapshotToMap,
-} from '../../firebase/firebase.utils';
 import { updateCollections } from '../../redux/shop/shop.actions';
 import WithSpinner from '../../components/with-spinner/with-spinner.component';
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
@@ -14,24 +10,10 @@ import ShopPageContainer from './shop.styles';
 const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
 const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 class ShopPage extends React.Component {
-  state = {
-    loading: true,
-  };
-  unsubscribeFromSnapshot = null;
-
-  componentDidMount() {
-    const { updateCollections } = this.props;
-    const collectionRef = firestore.collection('collections');
-    collectionRef.get().then(snapshot => {
-      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-      updateCollections(collectionsMap);
-      this.setState({ loading: false });
-    });
-  }
+  componentDidMount() {}
 
   render() {
     const { match } = this.props;
-    const { loading } = this.state;
     return (
       <ShopPageContainer>
         <Route
