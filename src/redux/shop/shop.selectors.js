9742c23d47243e0f84c91b028bdd94082ad0abaa
Anthony Curtis <tonydc1997@gmail.com>
2020-03-10 11:57:52 -0400

    feat: Create new selector w/ isFetching property

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -17,3 +17,8 @@ export const selectCollection = collectionUrlParam =>
   createSelector([selectCollections], collections =>
     collections ? collections[collectionUrlParam] : null
   );
+
+export const selectCollectionIsFetching = createSelector(
+  [selectShop],
+  shop => shop.isFetching
+);
