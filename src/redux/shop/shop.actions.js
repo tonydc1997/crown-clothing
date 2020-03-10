d541fe5ae2ed9aa9a7efaefa6e29b904894a4dd9
Anthony Curtis <tonydc1997@gmail.com>
2020-03-10 11:34:27 -0400

    feat: Create fetch...StartAsync vers.
    - Copy/paste async fetch call from shop.component.

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -3,3 +3,15 @@ import ShopActiontypes from './shop.types';
 export const fetchCollectionsStart = () => ({
   type: ShopActiontypes.FETCH_COLLECTIONS_START,
 });
+
+export const fetchCollectionsStartAsync = () => {
+  return dispatch => {
+    const collectionRef = firestore.collection('collections');
+
+    collectionRef.get().then(snapshot => {
+      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+      updateCollections(collectionsMap);
+      this.setState({ loading: false });
+    });
+  };
+};
