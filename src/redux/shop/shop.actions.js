9128708a5bcfba994a6e97ccc817e15725a7bb92
Anthony Curtis <tonydc1997@gmail.com>
2020-03-10 11:39:23 -0400

    feat: Create fetch...Success shopAction

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -8,6 +8,11 @@ export const fetchCollectionsStart = () => ({
   type: ShopActiontypes.FETCH_COLLECTIONS_START,
 });
 
+export const fetchCollectionsSuccess = () => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
+  payload: collectionsMap,
+});
+
 export const fetchCollectionsStartAsync = () => {
   return dispatch => {
     const collectionRef = firestore.collection('collections');
