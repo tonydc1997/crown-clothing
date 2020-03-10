83128adb80b02677ed0a7d56d195e5d68c022164
Anthony Curtis <tonydc1997@gmail.com>
2020-03-10 11:47:45 -0400

    feat: Create fetch...Failure shop.action

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -13,6 +13,11 @@ export const fetchCollectionsSuccess = collectionsMap => ({
   payload: collectionsMap,
 });
 
+export const fetchCollectionsFailure = errorMessage => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
+  payload: errorMessage,
+});
+
 export const fetchCollectionsStartAsync = () => {
   return dispatch => {
     const collectionRef = firestore.collection('collections');
