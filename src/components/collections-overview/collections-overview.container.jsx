25fa8e5129c59ee08c1f239c5c5cd15eb95376a0
Anthony Curtis <tonydc1997@gmail.com>
2020-03-13 08:43:12 -0400

    chore: Import necessary dependencies

---
+++ src/components/collections-overview/collections-overview.container.jsx
@@ -0,0 +1,5 @@
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { selectCollectionIsFetching } from '../../redux/shop/shop.selectors';
+import WithSpinner from '../with-spinner/with-spinner.component';
+import CollectionsOverview from './collections-overview.component';
