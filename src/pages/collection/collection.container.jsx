c301ba41454e6bbfface126ac660abd64e0ba56c
Anthony Curtis <tonydc1997@gmail.com>
2020-03-13 09:34:08 -0400

    chore: Import necessary dependencies

---
+++ src/pages/collection/collection.container.jsx
@@ -0,0 +1,6 @@
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { createStructuredSelector } from 'reselect';
+import { selectIsCollectionsLoaded } from '../../redux/shop/shop.selectors';
+import WithSpinner from '../../components/with-spinner/with-spinner.component';
+import CollectionPage from './collection.component';
