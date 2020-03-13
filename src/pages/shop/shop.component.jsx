0015c090a59276476b1ae68d8049354ead751cff
Anthony Curtis <tonydc1997@gmail.com>
2020-03-13 10:07:59 -0400

    feat: Replace stateful render prop w/ component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -23,12 +23,7 @@ class ShopPage extends React.Component {
         />
         <Route
           path={`${match.path}/:collectionId`}
-          render={props => (
-            <CollectionPageWithSpinner
-              isLoading={!isCollectionsLoaded}
-              {...props}
-            />
-          )}
+          component={CollectionPageContainer}
         />
       </ShopPageContainer>
     );
