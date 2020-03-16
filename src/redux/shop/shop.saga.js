955db60c97682ec5b2889764adc9a4b99fe07d69
Anthony Curtis <tonydc1997@gmail.com>
2020-03-16 11:59:23 -0400

    feat: Declare & export 2 saga functions

--- src/redux/shop/shop.saga.js
+++ src/redux/shop/shop.saga.js
@@ -1,2 +1,13 @@
 import { takeEvery } from 'redux-saga';
 import ShopActionTypes from './shop.types';
+
+export function* fetchCollectionsAsync() {
+  yield console.log("I'm being fired...");
+}
+
+export function* fetchCollectionsStart() {
+  yield takeEvery(
+    ShopActionTypes.FETCH_COLLECTIONS_START,
+    fetchCollectionsAsync
+  );
+}
