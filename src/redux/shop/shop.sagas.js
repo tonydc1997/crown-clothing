070d5317492475ccbb7d28bba6f0b9edf5d20307
Anthony Curtis <tonydc1997@gmail.com>
2020-03-16 12:30:31 -0400

    feat: Convert thunked shop.action to saga code
    -  Implement a try-catch block for shop.sagas.

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -11,6 +11,18 @@ import ShopActionTypes from './shop.types';
 
 export function* fetchCollectionsAsync() {
   yield console.log("I'm being fired...");
+
+  try {
+    const collectionRef = firestore.collection('collections');
+    const snapshot = yield collectionRef.get();
+    const collectionsMap = yield call(
+      convertCollectionsSnapshotToMap,
+      snapshot
+    );
+    yield put(fetchCollectionsSuccess(collectionsMap));
+  } catch (error) {
+    yield put(fetchCollectionsFailure(error.message));
+  }
 }
 
 export function* fetchCollectionsStart() {
