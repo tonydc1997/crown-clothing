953005c1e655ce12a64c9cc51ad34f8f63bd6fdd
Anthony Curtis <tonydc1997@gmail.com>
2020-03-17 12:43:17 -0400

    chore: Import needed firebase.utils deps
    - Create empty saga fn, signInWithGoogle

--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -1,5 +1,12 @@
 import { takeLatest, put } from 'redux-saga/effects';
 import UserActionTypes from './user.types';
+import {
+  auth,
+  googleProvider,
+  createUserProfileDocument,
+} from '../../firebase/firebase.utils';
+
+export function* signInWithGoogle() {}
 
 export function* onGoogleSignInStart() {
   yield takeLatest(UserActionTypes.GOOGLE_SIGN_IN_START);
