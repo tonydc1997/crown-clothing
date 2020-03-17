4461582ea0d227429cd7ff9d1b05ca19fba98deb
Anthony Curtis <tonydc1997@gmail.com>
2020-03-17 10:59:59 -0400

    feat: Ensure sagaMiddleware.run is called once
    - Implement rootSaga.

--- src/redux/store.js
+++ src/redux/store.js
@@ -15,6 +15,6 @@ if (process.env.NODE_ENV === 'development') {
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-sagaMiddlware.run(fetchCollectionsStart);
+sagaMiddlware.run(rootSaga);
 
 export const persistor = persistStore(store);
