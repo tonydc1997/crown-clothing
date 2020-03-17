df3b1a65de527f9294474e84c8a7e4ca543461f0
Anthony Curtis <tonydc1997@gmail.com>
2020-03-17 12:30:08 -0400

    feat: Add error to initial state

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -2,6 +2,7 @@ import UserActionTypes from './user.types';
 
 const INITIAL_STATE = {
   currentUser: null,
+  error: null,
 };
 const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
