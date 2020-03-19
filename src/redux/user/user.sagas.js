78d57896f2a52d642a2f022a1afa8fa533dcdc97
Anthony Curtis <tonydc1997@gmail.com>
2020-03-19 09:14:36 -0400

    feat: Implement try-catch block for user.saga

--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -6,7 +6,12 @@ import {
   createUserProfileDocument,
 } from '../../firebase/firebase.utils';
 
-export function* signInWithGoogle() {}
+export function* signInWithGoogle() {
+  try {
+    const userRef = yield auth.signInWithPopup(googleProvider);
+    console.log(userRef);
+  } catch (error) {}
+}
 
 export function* onGoogleSignInStart() {
   yield takeLatest(UserActionTypes.GOOGLE_SIGN_IN_START);
