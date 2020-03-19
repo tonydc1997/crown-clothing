0b43d35fbced44d5fcf273491a86ebbc1936491b
Anthony Curtis <tonydc1997@gmail.com>
2020-03-19 10:04:40 -0400

    feat: Yield success action for googleSignIn

--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -12,6 +12,9 @@ export function* signInWithGoogle() {
     const { user } = yield auth.signInWithPopup(googleProvider);
     const userRef = yield call(createUserProfileDocument, user);
     const userSnapshot = yield userRef.get();
+    yield put(
+      googleSignInSuccess({ id: userSnapshot.id, ...userSnapshot.data() })
+    );
   } catch (error) {}
 }
 
