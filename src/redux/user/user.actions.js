72efb5e7c4afd6f69ac900fc3bbd832ec9ceb9d1
Anthony Curtis <tonydc1997@gmail.com>
2020-03-19 09:34:43 -0400

    fix: Ensure type is consistent with fn name

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -6,16 +6,16 @@ export const setCurrentUser = user => ({
 });
 
 export const googleSignInStart = () => ({
-  type: UserActionTypes.email_SIGN_IN_START,
+  type: UserActionTypes.GOOGLE_SIGN_IN_START,
 });
 
 export const googleSignInSuccess = user => ({
-  type: UserActionTypes.email_SIGN_IN_SUCCESS,
+  type: UserActionTypes.GOOGLE_SIGN_IN_SUCCESS,
   payload: user,
 });
 
 export const googleSignInFailure = error => ({
-  type: UserActionTypes.email_SIGN_IN_FAILURE,
+  type: UserActionTypes.GOOGLE_SIGN_IN_FAILURE,
   payload: error,
 });
 
