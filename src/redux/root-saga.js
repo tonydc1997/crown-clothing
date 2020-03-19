8236ea83a0875a27311a2dfa872bb3f493ead78f
Anthony Curtis <tonydc1997@gmail.com>
2020-03-19 09:19:45 -0400

    feat: Call(userSagas) within rootSaga

--- src/redux/root-saga.js
+++ src/redux/root-saga.js
@@ -3,5 +3,5 @@ import { fetchCollectionsStart } from './shop/shop.sagas';
 import { userSagas } from './user/user.sagas';
 
 export default function* rootSaga() {
-  yield all([call(fetchCollectionsStart)]);
+  yield all([call(fetchCollectionsStart), call(userSagas)]);
 }
