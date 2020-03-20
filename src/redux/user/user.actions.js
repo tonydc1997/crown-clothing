8f421809a3cfee8627d3972b2eb0247b29e06a75
Anthony Curtis <tonydc1997@gmail.com>
2020-03-20 09:33:02 -0400

    chore: Rearrange user.action for consistency

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -4,6 +4,11 @@ export const googleSignInStart = () => ({
   type: UserActionTypes.GOOGLE_SIGN_IN_START,
 });
 
+export const emailSignInStart = emailAndPassword => ({
+  type: UserActionTypes.EMAIL_SIGN_IN_START,
+  payload: emailAndPassword,
+});
+
 export const signInSuccess = user => ({
   type: UserActionTypes.SIGN_IN_SUCCESS,
   payload: user,
@@ -12,9 +17,4 @@ export const signInSuccess = user => ({
 export const signInFailure = error => ({
   type: UserActionTypes.SIGN_IN_FAILURE,
   payload: error,
-});
-
-export const emailSignInStart = emailAndPassword => ({
-  type: UserActionTypes.EMAIL_SIGN_IN_START,
-  payload: emailAndPassword,
 });
