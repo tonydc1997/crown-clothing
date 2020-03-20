7580f0ccbb5e30d1b3e0847bc51523915ae93964
Anthony Curtis <tonydc1997@gmail.com>
2020-03-20 08:59:00 -0400

    feat: Dispatch fn/state to signIn Component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -79,6 +79,8 @@ class SignIn extends React.Component {
 
 const mapDispatchToProps = dispatch => ({
   googleSignInStart: () => dispatch(googleSignInStart()),
+  emailSignInStart: (email, password) =>
+    dispatch(emailSignInStart({ email, password })),
 });
 
 export default connect(null, mapDispatchToProps)(SignIn);
