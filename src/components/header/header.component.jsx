b2eed9aa490922d2d4eb3ef3de8a5aa749827891
Anthony Curtis <tonydc1997@gmail.com>
2020-03-23 12:10:20 -0400

    chore: Import signOutStart to header component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,6 +6,7 @@ import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import { selectCurrentUser } from '../../redux/user/user.selectors';
+import { signOutStart } from '../../redux/user/user.actions';
 import { auth } from '../../firebase/firebase.utils';
 import {
   HeaderContainer,
