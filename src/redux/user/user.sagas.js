b509c162954e5e8a0f8e12559b5d619b10158089
Anthony Curtis <tonydc1997@gmail.com>
2020-03-23 11:51:29 -0400

    feat: Ensure userSession listener is activated

--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -59,5 +59,9 @@ export function* onCheckUserSession() {
 }
 
 export function* userSagas() {
-  yield all([call(onGoogleSignInStart), call(onEmailSignInStart)]);
+  yield all([
+    call(onGoogleSignInStart),
+    call(onEmailSignInStart),
+    call(onCheckUserSession),
+  ]);
 }
