fd78d7423d7071e8d8e5621d1057ffc686a81dca
Anthony Curtis <tonydc1997@gmail.com>
2020-03-23 12:14:31 -0400

    feat: Destructure & use signOutStart onClick

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -15,7 +15,7 @@ import {
   OptionLink,
 } from './header.styles';
 
-const Header = ({ currentUser, hidden }) => (
+const Header = ({ currentUser, hidden, signOutStart }) => (
   <HeaderContainer>
     <LogoContainer to="/">
       <Logo className="logo" />
@@ -24,7 +24,7 @@ const Header = ({ currentUser, hidden }) => (
       <OptionLink to="/shop">SHOP</OptionLink>
       <OptionLink to="/shop">CONTACT</OptionLink>
       {currentUser ? (
-        <OptionLink as="div" onClick={() => auth.signOut()}>
+        <OptionLink as="div" onClick={signOutStart}>
           SIGN OUT
         </OptionLink>
       ) : (
