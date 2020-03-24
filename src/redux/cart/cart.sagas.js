c05e5b55cdbacc9de848e63d7eb1e6ba94b029ea
Anthony Curtis <tonydc1997@gmail.com>
2020-03-24 09:26:51 -0400

    feat: Create a root saga to handle all cartSagas

--- src/redux/cart/cart.sagas.js
+++ src/redux/cart/cart.sagas.js
@@ -1,3 +1,7 @@
 import { all, call, takeLatest, put } from 'redux-saga/effects';
 import UserActionTypes from '../user/user.types';
 import { clearCart } from './cart.actions';
+
+export function* cartSagas() {
+  yield all([]);
+}
