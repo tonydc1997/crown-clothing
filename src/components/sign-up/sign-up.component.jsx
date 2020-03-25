21731f1df9f2c691313fc334d524f5af82a63477
Anthony Curtis <tonydc1997@gmail.com>
2020-03-25 09:14:07 -0400

    feat/chore: Remove unused code
    - Remove the following:
      - Import statement
      - displayName & email destructuring
      - try-catch block within handleSubmit()

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import { auth, createUserProfileDocument } from '../../firebase/firebase.utils';
 import { SignUpContainer, Title } from './sign-up.styles';
 
 class SignUp extends React.Component {
@@ -18,26 +17,10 @@ class SignUp extends React.Component {
 
   handleSubmit = async event => {
     event.preventDefault();
-    const { displayName, email, password, confirmPassword } = this.state;
+    const { password, confirmPassword } = this.state;
     if (password !== confirmPassword) {
       alert("Passwords don't match.");
       return;
-    }
-
-    try {
-      const { user } = await auth.createUserWithEmailAndPassword(
-        email,
-        password
-      );
-      await createUserProfileDocument(user, { displayName });
-      this.setState({
-        displayName: '',
-        email: '',
-        password: '',
-        confirmPassword: '',
-      });
-    } catch (error) {
-      console.error(error);
     }
   };
 
