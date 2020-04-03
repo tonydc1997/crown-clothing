a17d59b503975ca32b2f5e1279185ba957305081
Anthony Curtis <tonydc1997@gmail.com>
2020-04-03 09:29:11 -0400

    feat: Serve static client/build when in production

--- server.js
+++ server.js
@@ -10,3 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+}
