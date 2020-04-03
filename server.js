b48ae8c24cfecda8a388df9450f36e798fa97db4
Anthony Curtis <tonydc1997@gmail.com>
2020-04-03 09:58:00 -0400

    feat: Finish stripe.charges as response to POST req

--- server.js
+++ server.js
@@ -30,4 +30,11 @@ app.post('/payment', (req, res) => {
     amount: req.body.amount,
     currency: 'usd',
   };
+  stripe.charges.create(body, (stripeErr, stripeRes) => {
+    if (stripeErr) {
+      res.status(500).send({ error: stripeErr });
+    } else {
+      res.status(200).send({ success: stripeRes });
+    }
+  });
 });
