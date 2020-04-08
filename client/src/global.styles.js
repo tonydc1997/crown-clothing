3134c70fd7449efa935699a7512704e49ffedcce
Anthony Curtis <tonydc1997@gmail.com>
2020-04-08 08:28:57 -0400

    feat: Copy/paste App.css styles to global.styles

--- client/src/global.styles.js
+++ client/src/global.styles.js
@@ -1,1 +1,17 @@
 import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+  font-family: 'Open Sans Condensed';
+}
+
+a {
+  text-decoration: none;
+  color: #171717;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+`;
