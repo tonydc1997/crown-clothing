10a4f2ac1a408ea2515a9c80845647d844884b77
Anthony Curtis <tonydc1997@gmail.com>
2020-04-09 11:42:42 -0400

    style: Ensure SignIn/Up Page and container is mobile ready
    - Ensure SignIn/Up is neatly aligned with one another.
    
    - Ensure sign-in btns are aligned and don't overlap.

--- client/src/components/sign-in/sign-in.styles.jsx
+++ client/src/components/sign-in/sign-in.styles.jsx
@@ -5,6 +5,17 @@ export const SignInContainer = styled.div`
   display: flex;
   flex-direction: column;
   margin-right: 4rem;
+
+  @media screen and (max-width: 520px) {
+    align-items: left;
+    width: 100%;
+    margin: unset;
+  }
+
+  @media screen and (min-width: 521px) and (max-width: 800px) {
+    width: 80%;
+    margin-left: 4rem;
+  }
 `;
 
 export const Title = styled.h2`
@@ -21,4 +32,12 @@ export const Content = styled.span`
 export const ButtonsContainer = styled.div`
   display: flex;
   justify-content: space-between;
+
+  @media screen and (max-width: 1400px) {
+    flex-direction: column;
+
+    button {
+      margin-bottom: 15px;
+    }
+  }
 `;
--- client/src/components/sign-up/sign-up.styles.jsx
+++ client/src/components/sign-up/sign-up.styles.jsx
@@ -9,6 +9,15 @@ export const SignUpContainer = styled.div`
     color: #486581;
     font-size: 18px;
   }
+
+  @media screen and (max-width: 520px) {
+    align-items: left;
+    width: 100%;
+  }
+
+  @media screen and (min-width: 521px) and (max-width: 800px) {
+    width: 80%;
+  }
 `;
 
 export const Title = styled.h2`
