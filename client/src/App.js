47a52c5f1c8cd4a064ac3ba0423645fd8a1a00e4
Anthony Curtis <tonydc1997@gmail.com>
2020-04-10 10:49:00 -0400

    feat: Dynamically import pages via lazy-loading

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,17 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import Header from './components/header/header.component';
-import Homepage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component';
-import CheckoutPage from './pages/checkout/checkout.component';
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import GlobalStyle from './global.styles';
 
 import { selectCurrentUser } from './redux/user/user.selectors';
 import { checkUserSession } from './redux/user/user.actions';
+
+const Homepage = lazy(() => import('./pages/homepage/homepage.component'));
+const ShopPage = lazy(() => import('./pages/shop/shop.component'));
+const CheckoutPage = lazy(() => import('./pages/checkout/checkout.component'));
+const SignInAndSignUpPage = lazy(() =>
+  import('./pages/sign-in-and-sign-up/sign-in-and-sign-up.component')
+);
 
 const App = ({ checkUserSession, currentUser }) => {
   useEffect(() => {
