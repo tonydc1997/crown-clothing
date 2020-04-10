4996fdf029abdb1840cabca962c227c8ee0b5f29
Anthony Curtis <tonydc1997@gmail.com>
2020-04-10 12:50:20 -0400

    feat: Import & use withRouter for CollectionPreview

--- client/src/components/collection-preview/collection-preview.component.jsx
+++ client/src/components/collection-preview/collection-preview.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import CollectionItem from '../collection-item/collection-item.component';
 import {
   CollectionPreviewContainer,
@@ -19,4 +20,4 @@ const CollectionPreview = ({ title, items }) => (
   </CollectionPreviewContainer>
 );
 
-export default CollectionPreview;
+export default withRouter(CollectionPreview);
