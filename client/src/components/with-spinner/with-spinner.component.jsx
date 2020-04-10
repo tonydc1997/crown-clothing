9fd36a3cbc2a6fe6164241db98454a596e61d582
Anthony Curtis <tonydc1997@gmail.com>
2020-04-10 11:04:18 -0400

    fix: Resolve filename typo
    - Have VSCode auto-fix import in with-spinner component.

--- client/src/components/with-spinner/with-spinner.component.jsx
+++ client/src/components/with-spinner/with-spinner.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Spinner from '../spinner/spinner.componenet';
+import Spinner from '../spinner/spinner.component';
 
 const WithSpinner = WrappedComponent => ({ isLoading, ...otherProps }) => {
   return isLoading ? <Spinner /> : <WrappedComponent {...otherProps} />;
