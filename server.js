5382d88586b7dd6360f68ab24e0497c0fedef0ca
Anthony Curtis <tonydc1997@gmail.com>
2020-04-14 12:56:46 -0400

    feat: Move compression call to production process

--- server.js
+++ server.js
@@ -9,13 +9,14 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
+  app.use(compression());
   app.use(express.static(path.join(__dirname, 'client/build')));
+
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
