feb6d56dfefbb8904675526dbb9a8f8dc9173560
Anthony Curtis <tonydc1997@gmail.com>
2020-04-14 11:54:46 -0400

    feat: Import * register/use serviceWorker

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/es/integration/react';
 import { store, persistor } from './redux/store';
+import * as serviceWorker from './serviceWorker';
 import './index.css';
 import App from './App';
 
@@ -17,3 +18,5 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
+
+serviceWorker.register();
